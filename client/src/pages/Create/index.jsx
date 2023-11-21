1cf8682746d22826cfa40235627f2e6462ecca23
ebrest993 <elliott.brest@gmail.com>
2023-11-21 11:51:46 -0600

    textarea added

--- client/src/pages/Create/index.jsx
+++ client/src/pages/Create/index.jsx
@@ -123,7 +123,7 @@ function BasicExample() {
   return (
     <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
+        <Form.Label>Subject</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
@@ -136,6 +136,10 @@ function BasicExample() {
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Example textarea</Form.Label>
+        <Form.Control as="textarea" rows={3} />
       </Form.Group>
       <Button variant="primary" type="submit">
         Submit
