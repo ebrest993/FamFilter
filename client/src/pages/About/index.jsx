d8b46e02ad7d0529fcf6fb5075a37652578e64d9
ebrest993 <elliott.brest@gmail.com>
2023-11-27 15:00:42 -0600

    about page created

---
+++ client/src/pages/About/index.jsx
@@ -0,0 +1,12 @@
+import './style.scss';
+
+export default function About() {
+  return (
+    <div id="profile-page">
+      <h1>about
+        <h6>how we got started</h6>
+      </h1>
+    </div>
+  );
+};
+
