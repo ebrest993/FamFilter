1baf884c1a6a3e081a02d203bc664f0edef7272f
ebrest993 <elliott.brest@gmail.com>
2023-11-29 18:13:36 -0600

    working dashboard backup

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,10 +1,11 @@
 import './style.scss';
 import React from 'react';
-import { gql, useQuery} from '@apollo/client';
+import { useQuery} from '@apollo/client';
 import {QUERY_THREADS} from '../../utils/queries.js';
 
 export default function Home() {
-  const { loading, error, data } = useQuery(QUERY_THREADS);
+  const { data } = useQuery(QUERY_THREADS);
+  const threads = data?.threads || [];
   console.log('heeeeeeeres Data: ', data);
   return (
     <div id="home-page">
@@ -12,11 +13,12 @@ export default function Home() {
         <h6>
           all announcements
         </h6>
-        {data?.threads.map(... => {
+        {threads.map((threads) => {
+          return (
           <p>
-          </p>
             {threads.messages[0].message}
-        })}
+          </p>
+        )})}
       </h1>
     </div>
   );
